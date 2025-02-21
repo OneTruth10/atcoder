748bcd8d70e40bde8f0b54be4a08726b25989097
OneTruth <firespiritkn10@gmail.com>
2025-02-21 17:01:14 +0000

    trying D problem

--- swap_to_gather.py
+++ swap_to_gather.py
@@ -2,19 +2,12 @@ n = input()
 s = input()
 s = list(s)
 n = int(n)
-left = 0
-longest = 0
-curr = 0
-con = False
-for i in range(n):
-    if s[i]=="1" and not con:
-        con = True
-        curr = i
-    elif s[i]=="0" and con:
-        con = False
-        if longest < i-curr:
-            longest = i-curr
-            left = curr
+count = 0
+ones = []
+for i in n:
+    if s[i] == "1":
+        count+=1
+        ones.append(i)
 
-
-print(f"{longest}, starting from: {left}")
+for i in range(n-count):
+    
