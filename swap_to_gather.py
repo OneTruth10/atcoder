67f21c18a85c928128a90a3f7ff3d491b2a3ae4f
OneTruth <firespiritkn10@gmail.com>
2025-03-06 17:27:16 +0000

    Resolved D from contest 392

--- swap_to_gather.py
+++ swap_to_gather.py
@@ -4,10 +4,15 @@ s = list(s)
 n = int(n)
 count = 0
 ones = []
-for i in n:
+length = 100_000_000_000
+for i in range(n):
     if s[i] == "1":
         count+=1
         ones.append(i)
 
+median = ones[len(ones)//2]
+result = 0
+for i, one in enumerate(ones):
+    result+= abs(median-one) - abs(len(ones)//2-i)
 
-
+print(result)
